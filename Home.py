52e933917bcb80dfdc6e76052c455bdf36f4cfce
WissamElJ <wissam.eljablaoui@gmail.com>
2024-03-16 19:32:49 +0300

    homepage

---
+++ Home.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# Projects")
+
+
+
+st.markdown(
+    """
+    Below you can see blablabla
+    
+    Happy exploring!
+
+    - 👉 **Master's Thesis** : [What Makes a Good Speech](https://www.google.com)
+    - 👉 **Dashboard Building** : [Kaggle Store Sales for Ecuador]
+    - 👉 **Big Data Project** : [Churn Prediction]
+    - 👉 **One more** : [menshouf]
+    - 👉 **Bachelor's Thesis** : [Bed Optimization & Management]
+    
+"""
+)
