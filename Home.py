41890d95f2b2234d5deb720e98a1d3ddfe35629f
WissamElJ <59507033+WissamElJ@users.noreply.github.com>
2024-03-20 19:00:25 +0100

    dash link

--- Home.py
+++ Home.py
@@ -11,12 +11,10 @@ st.write("# Projects")
 
 st.markdown(
     """
-    Below you can see blablabla
-    
     Happy exploring!
 
     - 👉 **Master's Thesis** : [What Makes a Good Speech](https://wissam-thesis.streamlit.app/)
-    - 👉 **Dashboard Building** : [Kaggle Store Sales for Ecuador]()
+    - 👉 **Dashboard Building** : [Kaggle Store Sales for Ecuador](https://wissam-homepage.streamlit.app/)
     - 👉 **Big Data Project** : [Churn Prediction]
     - 👉 **Bachelor's Thesis** : [Bed Optimization & Management](https://wissam-capstone.streamlit.app/)
     - 👉 **Many more to come!**
